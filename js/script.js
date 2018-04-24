c73b3de77f611d3cbcb097ca6bed31a40808959e
Jim <jim@advancebass.com>
2018-04-23 22:01:31 -0700

    add melody and bassline up to bar 11, some sound edits, still trying to figure note note firing

--- js/script.js
+++ js/script.js
@@ -1,142 +1,164 @@
-Tone.Transport.bpm.value = 60;
+document.addEventListener("DOMContentLoaded", function() {
+  Tone.Transport.bpm.value = 120;
 
-/**
- * Melody
- */
-var mainMelody = new Tone.Synth({
-  volume: -0.5,
-  oscillator: {
-    type: "amtriangle",
-    harmonicity: 0.5,
-    modulationType: "sine"
-  },
-  envelope: {
-    attackCurve: "exponential",
-    attack: 0.05,
-    decay: 0.2,
-    sustain: 0.2,
-    release: 1.5
-  },
-  portamento: 0.01
-}).toMaster();
+  // let reverb = new Tone.JCReverb(0.6).connect(Tone.Master);
+  // let reverb2 = new Tone.JCReverb(0.1).connect(Tone.Master);
 
-var mainMelodyPart = new Tone.Sequence(
-  function(time, note) {
-    mainMelody.triggerAttackRelease(note, "16n", time);
-  },
-  [
-    "Bb3",
-    [[null, null], ["Bb3", "A3"]],
-    "Bb3",
-    [[null, null], ["Bb3", "A3"]],
-    [["Bb3"], [null, "C4"]],
-    [["C4"], [null, "Eb4"]],
-    "Eb4",
-    [null, ["D4", "Bb3"]], // beat 8
-    [["C4"], [null, "A3"]],
-    [["F3"], ["D4", "Bb3"]],
-    "C4",
-    [[null], ["F4", "Bb3"]],
-    [["Eb4"], [null, "D4"]],
-    [["D4"], [null, "C4"]],
-    "C4",
-    null, // beat 16
-    "Bb3",
-    [["A3"], ["Bb4", "A4"]], // beat 19
-    "Bb4",
-    [[null, null], ["Bb4", "A4"]],
-    "Bb4", // beat 26
-    [[null, null], ["Bb4", "A4"]],
-    [["Bb4"], [null, "C5"]],
-    [["C5"], [null, "Eb5"]],
-    "Eb5",
-    [null, ["D5", "Bb4"]],
-    [["C5"], [null, "A4"]],
-    [["F4"], ["D5", "Bb4"]],
-    "C5",
-    [[null], ["F5", "Bb4"]],
-    [["Eb5"], [null, "D5"]],
-    [["D5"], [null, "C5"]],
-    "C5"
-  ]
-).start(0);
+  /**
+   * Melody
+   */
+  let mainMelody = new Tone.Synth({
+    volume: -15,
+    oscillator: {
+      type: "amtriangle",
+      harmonicity: 0.5,
+      modulationType: "square"
+    },
+    envelope: {
+      attackCurve: "exponential",
+      attack: 0.1,
+      decay: 0.5,
+      sustain: 0.5,
+      release: 2
+    },
+    portamento: 0.02
+  }).toMaster();
 
-mainMelodyPart.probability = 0.9;
-// mainMelodyPart.loopEnd = "4m";
+  let mainMelodyPart = new Tone.Sequence(
+    function(time, note) {
+      mainMelody.triggerAttackRelease(note, "0:0:1", time);
+    },
+    [
+      "Bb3", // bar 1
+      [[null, null], ["Bb3", "A3"]],
+      "Bb3",
+      [[null, null], ["Bb3", "A3"]],
+      [["Bb3"], [null, "C4"]], // bar 2
+      [["C4"], [null, "Eb4"]],
+      "Eb4",
+      [null, ["D4", "Bb3"]],
+      [["C4"], [null, "A3"]], // bar 3
+      [["F3"], ["D4", "Bb3"]],
+      "C4",
+      [[null], ["F4", "Bb3"]],
+      [["Eb4"], [null, "D4"]], // bar 4, 6 beats
+      [["D4"], [null, "C4"]],
+      "C4",
+      null,
+      "Bb3",
+      [["A3"], ["Bb4", "A4"]],
+      "Bb4", // bar 5
+      [[null, null], ["Bb4", "A4"]],
+      "Bb4",
+      [[null, null], ["Bb4", "A4"]],
+      [["Bb4"], [null, "C5"]], // bar 6
+      [["C5"], [null, "Eb5"]],
+      "Eb5",
+      [null, ["D5", "Bb4"]],
+      [["C5"], [null, "A4"]], // bar 7
+      [["F4"], ["D5", "Bb4"]],
+      "C5",
+      [[null], ["F5", "Bb4"]],
+      [["Eb5"], [null, "D5"]], // bar 8
+      [["D5"], [null, "C5"]],
+      "C5",
+      [null, ["Bb5", "A5"]],
+      ["Bb5", "F5"], // bar 9
+      ["Eb5", ["Bb5", "A5"]],
+      ["Bb5", "F5"],
+      ["Eb5", ["Bb5", "A5"]],
+      [["A5","Bb5"],null], // bar 10
+      ["F5","Bb4"],
+      "Ab5",
+      [null,["Bb5","A5"]],
+    ]
+  ).start();
 
-/**
- * Bass Melody
- */
-var bassMelody = new Tone.MonoSynth({
-  volume: -15,
-  oscillator: {
-    type: "square8"
-  },
-  envelope: {
-    attack: 0.05,
-    decay: 0.3,
-    sustain: 0.4,
-    release: 0.8
-  },
-  filterEnvelope: {
-    attack: 0.001,
-    decay: 0.7,
-    sustain: 0.1,
-    release: 0.8,
-    baseFrequency: 300,
-    octaves: 4
-  }
-}).toMaster();
+  mainMelodyPart.probability = 0.9;
+  // mainMelodyPart.loopEnd = "4m";
 
-var bassMelodyPart = new Tone.Sequence(
-  function(time, note) {
-    bassMelody.triggerAttackRelease(note, "4n", time);
-  },
-  [
-    "Bb2",
-    null,
-    "Eb2",
-    null,
-    "Bb2",
-    null,
-    "Bb2",
-    ["Bb2", "Bb2"], // beat 8
-    "F2",
-    ["F2", "Bb2"],
-    "F2",
-    [null, "D2"],
-    ["Eb2", [null, "F2"]],
-    ["F2", [null, "F2"]],
-    "F2",
-    "F2", // beat 16
-    [null, ["F2"]],
-    [null, ["F2"]],
-    "Bb2", // beat 19
-    null,
-    "Eb2",
-    null,
-    "Bb2",
-    null,
-    "Bb2",
-    ["Bb2", "Bb2"], // beat 26
-    "F2",
-    ["F2", "Bb2"],
-    "F2",
-    [null, "D2"],
-    ["Eb2", [null, "F2"]],
-    ["F2", [null, "F2"]],
-    "F2",
-    "F2" // beat 16
-  ]
-).start(0);
+  /**
+   * Bass Melody
+   */
+  let bassMelody = new Tone.MonoSynth({
+    volume: -20,
+    oscillator: {
+      type: "square"
+    },
+    envelope: {
+      attack: 0.05,
+      decay: 0.3,
+      sustain: 0.4,
+      release: 0.8
+    },
+    filterEnvelope: {
+      attack: 0.001,
+      decay: 0.7,
+      sustain: 0.1,
+      release: 0.8,
+      baseFrequency: 200,
+      octaves: 4
+    }
+  }).toMaster();
 
-bassMelodyPart.probability = 0.9;
+  let bassMelodyPart = new Tone.Sequence(
+    function(time, note) {
+      bassMelody.triggerAttackRelease(note, "0:1", time);
+    },
+    [
+      "Bb1", // bar 1
+      null,
+      "Eb1",
+      null,
+      "Bb1", //  bar 2
+      null,
+      "Bb1",
+      ["Bb1", "Bb1"],
+      "F1", // bar 3
+      ["F1", "Bb1"],
+      "F1",
+      [null, "D1"],
+      ["Eb1", [null, "F1"]], // bar 4, 6 beats
+      ["F1", [null, "F1"]],
+      "F1",
+      "F1",
+      [null, ["F1"]],
+      [null, ["F1"]],
+      "Bb1", // bar 5
+      "Bb1",
+      "Eb1",
+      "Eb1",
+      "Bb1", // bar 6
+      ["Bb1", [null, "F2"]],
+      "Bb1",
+      ["Bb1", "Bb1"],
+      "F1", // bar 7
+      ["F2", "F2"],
+      "F1",
+      ["F1", "D1"],
+      ["G1", [null, "F1"]], // bar 8
+      ["F1", [null, "F1"]],
+      "F1",
+      "F1",
+      "Bb1", // bar 9
+      ["Eb1","Eb1"],
+      "Bb1",
+      ["Eb1","Eb1"],
+      "Bb1", // bar 10
+      "Bb2",
+      ["Eb1","Eb1"],
+      ["Eb1","F1"]
+    ]
+  ).start();
 
-/**
- * Play Controls
- */
-document.querySelector("body").addEventListener("click", function(e) {
-  Tone.Transport.start();
+  bassMelodyPart.probability = 0.9;
+
+  /**
+   * Play Controls
+   */
+  document.querySelector("body").addEventListener("click", function(e) {
+    Tone.Transport.start("+1");
+  });
+
+  // Tone.Transport.start("+0.1");
 });
-
-// Tone.Transport.start("+0.1");
