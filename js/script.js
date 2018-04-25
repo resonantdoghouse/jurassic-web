a28ec08e4b73e54058e97ce02cab0d123b3aee4f
Jim <jim@advancebass.com>
2018-04-24 21:01:54 -0700

    switch from codekit to gulp, organize files and setup es6 gulp-babel

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   Tone.Transport.bpm.value = 60;
 
   // let reverb = new Tone.JCReverb(0.6).connect(Tone.Master);
@@ -10,12 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
   let mainMelody = new Tone.Synth({
     volume: -15,
     oscillator: {
-      type: "amtriangle",
+      type: 'amtriangle',
       harmonicity: 0.5,
-      modulationType: "square"
+      modulationType: 'square'
     },
     envelope: {
-      attackCurve: "exponential",
+      attackCurve: 'exponential',
       attack: 0.1,
       decay: 0.5,
       sustain: 0.5,
@@ -26,56 +26,56 @@ document.addEventListener("DOMContentLoaded", function() {
 
   let mainMelodyPart = new Tone.Sequence(
     function(time, note) {
-      mainMelody.triggerAttackRelease(note, "0:0:1", time);
+      mainMelody.triggerAttackRelease(note, '0:0:1', time);
     },
     [
-      "Bb3", // bar 1
-      [[null, null], ["Bb3", "A3"]],
-      "Bb3",
-      [[null, null], ["Bb3", "A3"]],
-      [["Bb3"], [null, "C4"]], // bar 2
-      [["C4"], [null, "Eb4"]],
-      "Eb4",
-      [null, ["D4", "Bb3"]],
-      [["C4"], [null, "A3"]], // bar 3
-      [["F3"], ["D4", "Bb3"]],
-      "C4",
-      [[null], ["F4", "Bb3"]],
-      [["Eb4"], [null, "D4"]], // bar 4, 6 beats
-      [["D4"], [null, "C4"]],
-      "C4",
+      'Bb3', // bar 1
+      [[null, null], ['Bb3', 'A3']],
+      'Bb3',
+      [[null, null], ['Bb3', 'A3']],
+      [['Bb3'], [null, 'C4']], // bar 2
+      [['C4'], [null, 'Eb4']],
+      'Eb4',
+      [null, ['D4', 'Bb3']],
+      [['C4'], [null, 'A3']], // bar 3
+      [['F3'], ['D4', 'Bb3']],
+      'C4',
+      [[null], ['F4', 'Bb3']],
+      [['Eb4'], [null, 'D4']], // bar 4, 6 beats
+      [['D4'], [null, 'C4']],
+      'C4',
       null,
-      "Bb3",
-      [["A3"], ["Bb4", "A4"]],
-      "Bb4", // bar 5
-      [[null, null], ["Bb4", "A4"]],
-      "Bb4",
-      [[null, null], ["Bb4", "A4"]],
-      [["Bb4"], [null, "C5"]], // bar 6
-      [["C5"], [null, "Eb5"]],
-      "Eb5",
-      [null, ["D5", "Bb4"]],
-      [["C5"], [null, "A4"]], // bar 7
-      [["F4"], ["D5", "Bb4"]],
-      "C5",
-      [[null], ["F5", "Bb4"]],
-      [["Eb5"], [null, "D5"]], // bar 8
-      [["D5"], [null, "C5"]],
-      "C5",
-      [null, ["Bb5", "A5"]],
-      ["Bb5", "F5"], // bar 9
-      ["Eb5", ["Bb5", "A5"]],
-      ["Bb5", "F5"],
-      ["Eb5", ["Bb5", "A5"]],
-      [["A5","Bb5"],null], // bar 10
-      ["F5","Bb4"],
-      "Ab5",
-      [null,["Bb5","A5"]],
+      'Bb3',
+      [['A3'], ['Bb4', 'A4']],
+      'Bb4', // bar 5
+      [[null, null], ['Bb4', 'A4']],
+      'Bb4',
+      [[null, null], ['Bb4', 'A4']],
+      [['Bb4'], [null, 'C5']], // bar 6
+      [['C5'], [null, 'Eb5']],
+      'Eb5',
+      [null, ['D5', 'Bb4']],
+      [['C5'], [null, 'A4']], // bar 7
+      [['F4'], ['D5', 'Bb4']],
+      'C5',
+      [[null], ['F5', 'Bb4']],
+      [['Eb5'], [null, 'D5']], // bar 8
+      [['D5'], [null, 'C5']],
+      'C5',
+      [null, ['Bb5', 'A5']],
+      ['Bb5', 'F5'], // bar 9
+      ['Eb5', ['Bb5', 'A5']],
+      ['Bb5', 'F5'],
+      ['Eb5', ['Bb5', 'A5']],
+      [['A5', 'Bb5'], null], // bar 10
+      ['F5', 'Bb4'],
+      'Ab5',
+      [null, ['Bb5', 'A5']]
     ]
   ).start();
 
   mainMelodyPart.probability = 0.9;
-  // mainMelodyPart.loopEnd = "4m";
+  // mainMelodyPart.loopEnd = '4m';
 
   /**
    * Bass Melody
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let bassMelody = new Tone.MonoSynth({
     volume: -20,
     oscillator: {
-      type: "square"
+      type: 'square'
     },
     envelope: {
       attack: 0.05,
@@ -103,51 +103,51 @@ document.addEventListener("DOMContentLoaded", function() {
 
   let bassMelodyPart = new Tone.Sequence(
     function(time, note) {
-      bassMelody.triggerAttackRelease(note, "0:1", time);
+      bassMelody.triggerAttackRelease(note, '0:1', time);
     },
     [
-      "Bb1", // bar 1
+      'Bb1', // bar 1
       null,
-      "Eb1",
+      'Eb1',
       null,
-      "Bb1", //  bar 2
+      'Bb1', //  bar 2
       null,
-      "Bb1",
-      ["Bb1", "Bb1"],
-      "F1", // bar 3
-      ["F1", "Bb1"],
-      "F1",
-      [null, "D1"],
-      ["Eb1", [null, "F1"]], // bar 4, 6 beats
-      ["F1", [null, "F1"]],
-      "F1",
-      "F1",
-      [null, ["F1"]],
-      [null, ["F1"]],
-      "Bb1", // bar 5
-      "Bb1",
-      "Eb1",
-      "Eb1",
-      "Bb1", // bar 6
-      ["Bb1", [null, "F2"]],
-      "Bb1",
-      ["Bb1", "Bb1"],
-      "F1", // bar 7
-      ["F2", "F2"],
-      "F1",
-      ["F1", "D1"],
-      ["G1", [null, "F1"]], // bar 8
-      ["F1", [null, "F1"]],
-      "F1",
-      "F1",
-      "Bb1", // bar 9
-      ["Eb1","Eb1"],
-      "Bb1",
-      ["Eb1","Eb1"],
-      "Bb1", // bar 10
-      "Bb2",
-      ["Eb1","Eb1"],
-      ["Eb1","F1"]
+      'Bb1',
+      ['Bb1', 'Bb1'],
+      'F1', // bar 3
+      ['F1', 'Bb1'],
+      'F1',
+      [null, 'D1'],
+      ['Eb1', [null, 'F1']], // bar 4, 6 beats
+      ['F1', [null, 'F1']],
+      'F1',
+      'F1',
+      [null, ['F1']],
+      [null, ['F1']],
+      'Bb1', // bar 5
+      'Bb1',
+      'Eb1',
+      'Eb1',
+      'Bb1', // bar 6
+      ['Bb1', [null, 'F2']],
+      'Bb1',
+      ['Bb1', 'Bb1'],
+      'F1', // bar 7
+      ['F2', 'F2'],
+      'F1',
+      ['F1', 'D1'],
+      ['G1', [null, 'F1']], // bar 8
+      ['F1', [null, 'F1']],
+      'F1',
+      'F1',
+      'Bb1', // bar 9
+      ['Eb1', 'Eb1'],
+      'Bb1',
+      ['Eb1', 'Eb1'],
+      'Bb1', // bar 10
+      'Bb2',
+      ['Eb1', 'Eb1'],
+      ['Eb1', 'F1']
     ]
   ).start();
 
@@ -156,9 +156,9 @@ document.addEventListener("DOMContentLoaded", function() {
   /**
    * Play Controls
    */
-  document.querySelector("body").addEventListener("click", function(e) {
-    Tone.Transport.start("+1");
+  document.querySelector('body').addEventListener('click', function() {
+    Tone.Transport.start('+1');
   });
 
-  // Tone.Transport.start("+0.1");
+  // Tone.Transport.start('+0.1');
 });
